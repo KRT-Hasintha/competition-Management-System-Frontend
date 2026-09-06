8db7cf3462e7fc93b9724d8b731442d293433c16
KRT-Hasintha <thushhasi12@gmail.com>
2026-09-06 12:42:56 +0530

    fix css import ts error

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,26 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+
+// // TypeScript does not resolve CSS side-effect imports without a stylesheet declaration.
+// // @ts-expect-error CSS is handled by the bundler.
+// import "./index.css";
+// import "./i18n";
+
+// ReactDOM.createRoot(
+//   document.getElementById("root")!
+// ).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-// TypeScript does not have declarations for CSS side-effect imports in this setup.
-// @ts-expect-error CSS is handled by the bundler at runtime.
+
+// @ts-ignore
 import "./index.css";
 import "./i18n";
 
